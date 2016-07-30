9ceb25f9c3394d16dfe4272bde6e856149862441
wendyvdklooster <wendyvanderklooster@upcmail.nl>
2016-07-30 16:40:43 +0200

    DAO's voor FB gemaakt. update werkt niet

--- Documents/NetBeansProjects/WebshopJavaApplicatie/src/main/java/DAOs/Impl/ArtikelDAOFB.java
+++ Documents/NetBeansProjects/WebshopJavaApplicatie/src/main/java/DAOs/Impl/ArtikelDAOFB.java
@@ -7,6 +7,12 @@ package DAOs.Impl;
 
 import DAOs.Interface.ArtikelDAOInterface;
 import POJO.Artikel;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -15,42 +21,261 @@ import java.util.ArrayList;
  */
 public class ArtikelDAOFB implements ArtikelDAOInterface {
 
+    Connection con;
+    ResultSet rs;
+    PreparedStatement pstmt;
+    Statement st;
+    String driver = "org.firebirdsql.jdbc.FBDriver";
+    String url = "jdbc:firebirdsql:localhost/3050:C:\\FBDB\\fbdb.FDB";
+    String gebruikersNaam = "Anjewe";
+    String wachtwoord = "Koetjes";
+    
+    
     @Override
     public ArrayList<Artikel> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        ArrayList<Artikel> artikelList = new ArrayList<>();
+        
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+            
+            String sqlQuery = "select * from artikel";
+            
+            pstmt = con.prepareStatement(sqlQuery);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                // get the fields from one artikel and store it in an Artikel object
+                Artikel artikel = new Artikel();
+                artikel.setArtikelId(rs.getInt("artikel_id"));
+                artikel.setArtikelNaam(rs.getString("artikelnaam"));
+                artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
+
+                // add the artikel in the list
+                artikelList.add(artikel);
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Data search failed");
+        }
+
+       return artikelList;  
     }
 
+    
     @Override
     public Artikel findByArtikelID(int artikelID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Artikel artikel = new Artikel();
+        
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+            
+            String sqlQuery = "select * from artikel where artikel_id = ? ";
+            
+            pstmt = con.prepareStatement(sqlQuery);
+            pstmt.setInt(1, artikelID);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                artikel.setArtikelId(rs.getInt("artikel_id"));
+                artikel.setArtikelNaam(rs.getString("artikelnaam"));
+                artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
+            }
+            else {
+                throw new NullPointerException(); // is nog nodig, of kan gewoon printstatement in else?
+            }
+        } 
+        catch (NullPointerException ex) {
+            System.out.println("Dit artikel naam bestaat niet, probeer opnieuw. ");
+        } 
+        catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Data search failed");
+        }
+        
+        return artikel;
     }
 
+    
     @Override
     public Artikel findByArtikelNaam(String artikelNaam) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Artikel artikel = new Artikel();
+        
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+            
+            String SQLZoeken = "select * from artikel where artikelnaam = ? ";
+            
+            pstmt = con.prepareStatement(SQLZoeken);
+            pstmt.setString(1, artikelNaam);
+            rs = pstmt.executeQuery(SQLZoeken);
+            
+            if (rs.next()) {
+                artikel.setArtikelId(rs.getInt("artikel_id"));
+                artikel.setArtikelNaam(rs.getString("artikelnaam"));
+                artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
+            } 
+            else {
+                throw new NullPointerException(); // is nog nodig, of kan gewoon printstatement in else?
+            }
+        } 
+        catch (NullPointerException ex) {
+            System.out.println("Dit artikel naam bestaat niet, probeer opnieuw. ");
+        } 
+        catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Data search failed");
+        }
+        
+      return artikel;
     }
 
+    
     @Override
     public Artikel findByArtikelPrijs(double artikelPrijs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        
+        Artikel artikel = new Artikel();
+        
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+            
+            String sqlQuery = "select * from artikel where artikel_prijs = ? " ;
+            
+            pstmt = con.prepareStatement(sqlQuery);
+            pstmt.setDouble(1, artikelPrijs);
+            rs = pstmt.executeQuery();
 
-    @Override
+            if (rs.next()) {
+                artikel.setArtikelId(rs.getInt("artikel_id"));
+                artikel.setArtikelNaam(rs.getString("artikelnaam"));
+                artikel.setArtikelPrijs(rs.getDouble("artikel_prijs"));
+            }
+            else {
+                throw new NullPointerException(); // is nog nodig, of kan gewoon printstatement in else?
+            }
+        } 
+        catch (NullPointerException ex) {
+            System.out.println("Dit artikel naam bestaat niet, probeer opnieuw.");    
+        } 
+        catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Data search failed");
+        }
+        
+     return artikel;
+ }
+
+    
+    @Override //nog doen: AANPASSEN!
     public Artikel insertArtikel(String artikelNaam, double artikelPrijs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        Artikel artikel = new Artikel();
+        int artikelId = 0;
+        
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+            
+            String sqlUpdate = "insert into artikel (artikelnaam, artikel_prijs)"
+                    + "values (?, ?)";
+            
+            pstmt = con.prepareStatement(sqlUpdate, Statement.RETURN_GENERATED_KEYS);
+           
+            pstmt.setString(1, artikelNaam);
+            pstmt.setDouble(2, artikelPrijs);
+
+            pstmt.executeUpdate();
+            
+            rs = pstmt.getGeneratedKeys();
+            if (rs.isBeforeFirst()) {
+                if (rs.next()) {
+                    artikelId = rs.getInt(1);
+                }
+            }
+            
+            artikel.setArtikelId(artikelId);
+            artikel.setArtikelNaam(artikelNaam);
+            artikel.setArtikelPrijs(artikelPrijs);
+            
+        }catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Data entry failed.");            
+        }                
+        
+      return artikel; 
     }
 
+    
     @Override
-    public boolean deleteArtikel(int artikelID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean deleteArtikel(int artikelId) {
+        
+        boolean isDeleted = false;
+        
+        try {
+           Class.forName(driver);
+           con = DriverManager.getConnection(url, gebruikersNaam, wachtwoord);
+           
+           String artikelZoeken = "select * from artikel where artikel_id = ? ";
+           
+           pstmt = con.prepareStatement(artikelZoeken);
+           pstmt.setInt(1, artikelId);
+           rs = pstmt.executeQuery(artikelZoeken);
+           
+           boolean artikelFound = rs.next();
+           
+           if (artikelFound) {
+               
+                String sqlUpdate = "delete from artikel where artikel_id = ?";
+                
+                pstmt = con.prepareStatement(sqlUpdate);
+                pstmt.setInt(1, artikelId);
+                pstmt.executeUpdate(); 
+                isDeleted = true;
+                
+            }
+
+        }
+        catch (SQLException | ClassNotFoundException ex) {
+             System.err.println(ex.getMessage());
+        }
+        
+       return isDeleted;
     }
 
+    
     @Override
     public int deleteAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        
+        int rowsAffected = 0; 
+        
+        try{  
+            
+        Class.forName(driver);
+             // create a sql date object so we can use it in our INSERT statement
+             try (Connection conn = DriverManager.getConnection(url, gebruikersNaam, wachtwoord)) {
+                 // create a sql date object so we can use it in our INSERT statement
+                 
+                 // the mysql insert statement
+                 String sqlQuery = "delete from artikel";                         
+                 
+                 // create the mysql insert preparedstatement
+                 PreparedStatement preparedStmt = conn.prepareStatement(sqlQuery);
+                                                 
+                 // execute the preparedstatement
+                 rowsAffected = preparedStmt.executeUpdate();   
+                 
+            }
+        }catch (ClassNotFoundException | SQLException e){    
+            System.err.println(e.getMessage());
+        }  
+        
+     return rowsAffected;
+     
+   }
 
-    @Override
+    
+    @Override // nog doen
     public boolean update(Artikel artikel) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
--- Documents/NetBeansProjects/WebshopJavaApplicatie/src/main/java/DAOs/Impl/KlantDAOFB.java
+++ Documents/NetBeansProjects/WebshopJavaApplicatie/src/main/java/DAOs/Impl/KlantDAOFB.java
@@ -7,7 +7,16 @@ package DAOs.Impl;
 
 import DAOs.Interface.KlantDAOInterface;
 import POJO.Klant;
+import POJO.Klant.KlantBuilder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,49 +24,285 @@ import java.util.ArrayList;
  */
 public class KlantDAOFB implements KlantDAOInterface {
 
+    String driver = "org.firebirdsql.jdbc.FBDriver";
+    String url = "jdbc:firebirdsql:localhost/3050:C:\\FBDB\\fbdb.FDB";
+
+    String user = "Anjewe";
+    String pw = "Koetjes";
+    Connection con;
+    ResultSet rs;
+    PreparedStatement stmt;
+
+    KlantBuilder klantBuilder = new KlantBuilder();
+
     @Override
     public ArrayList<Klant> findAllKlanten() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Klant> klantenLijst = new ArrayList<>();
+
+        try {
+            //load driver
+            Class.forName(driver);
+            //establish a connection
+            con = DriverManager.getConnection(url, user, pw);
+
+            String sqlQuery = "SELECT Klant_id, voornaam, achternaam, tussenvoegsel, email FROM Klant";  // select: wildcard can be used*
+
+            stmt = con.prepareStatement(sqlQuery);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                klantBuilder.klantId(rs.getInt("klant_id"));
+                klantBuilder.voornaam(rs.getString("voornaam"));
+                klantBuilder.achternaam(rs.getString("achternaam"));
+                klantBuilder.tussenvoegsel(rs.getString("tussenvoegsel"));
+                klantBuilder.email(rs.getString("email"));
+
+                // build Klant
+                Klant klant = klantBuilder.build();
+                //voeg klant toe aan lijst
+                klantenLijst.add(klant);
+            }
+            con.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(KlantDAOSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return klantenLijst;
     }
 
     @Override
     public Klant findByKlantId(int klantId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        Klant klant = new Klant(klantBuilder);
+
+        try {
+            //load driver
+            Class.forName(driver);
+            //establish a connection
+            con = DriverManager.getConnection(url, user, pw);
+
+            String sqlQuery = "select * from Klant where klant_id = ? ";
+            stmt = con.prepareStatement(sqlQuery);
+
+            stmt.setInt(1, klantId);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                klantBuilder.klantId(rs.getInt("klant_id"));
+                klantBuilder.voornaam(rs.getString("voornaam"));
+                klantBuilder.achternaam(rs.getString("achternaam"));
+                klantBuilder.tussenvoegsel(rs.getString("tussenvoegsel"));
+                klantBuilder.email(rs.getString("email"));
+
+                // build Klant
+                klant = klantBuilder.build();
+
+            }
+            con.close();
+        } catch (SQLException | ClassNotFoundException ex) {
+        }
+
+        return klant;
     }
 
     @Override
     public Klant findByVoorNaamAchterNaam(String voorNaam, String achterNaam) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        Klant klant = new Klant(klantBuilder);
+
+        try {
+            //load driver
+            Class.forName(driver);
+            //establish a connection
+            con = DriverManager.getConnection(url, user, pw);
+
+            String sqlQuery = "select * from Klant where voorNaam = ? and achterNaam = ? ";
+            stmt = con.prepareStatement(sqlQuery);
+
+            stmt.setString(1, voorNaam);
+            stmt.setString(2, achterNaam);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                klantBuilder.klantId(rs.getInt("klant_id"));
+                klantBuilder.voornaam(rs.getString("voornaam"));
+                klantBuilder.achternaam(rs.getString("achternaam"));
+                klantBuilder.tussenvoegsel(rs.getString("tussenvoegsel"));
+                klantBuilder.email(rs.getString("email"));
+
+                // build Klant
+                klant = klantBuilder.build();
+
+            }
+            con.close();
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return klant;
     }
 
     @Override
     public Klant findByEmail(String email) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
-    @Override
-    public Klant insertKlant(Klant klant) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Klant klant = new Klant(klantBuilder);
+
+        try {
+            //load driver
+            Class.forName(driver);
+            //establish a connection
+            con = DriverManager.getConnection(url, user, pw);
+
+            String sqlQuery = "select * Klant where email = ? ";
+            stmt = con.prepareStatement(sqlQuery);
+
+            stmt.setString(1, email);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                klantBuilder.klantId(rs.getInt("klant_id"));
+                klantBuilder.voornaam(rs.getString("voornaam"));
+                klantBuilder.achternaam(rs.getString("achternaam"));
+                klantBuilder.tussenvoegsel(rs.getString("tussenvoegsel"));
+                klantBuilder.email(rs.getString("email"));
+
+                // build Klant
+                klant = klantBuilder.build();
+            }
+            con.close();
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return klant;
     }
 
     @Override
     public boolean deleteByKlantId(int klantId) {
+        boolean deleted = false;
+
+        try {
+            Class.forName(driver);
+            // create a sql date object so we can use it in our INSERT statement
+            try (Connection conn = DriverManager.getConnection(url, user, pw)) {
+                // create a sql date object so we can use it in our INSERT statement
+
+                // the mysql insert statement.first parent, than child
+                String sqlQuery = "delete from klant where klant_id =  ? ";
+
+                // create the mysql insert preparedstatement
+                PreparedStatement preparedStmt = conn.prepareStatement(sqlQuery);
+                preparedStmt.setInt(1, klantId);
+                // execute the preparedstatement
+                int rowsAffected = preparedStmt.executeUpdate();
+                if (rowsAffected != 0) {
+                    deleted = true;
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }
+        return deleted;
+    }
+    
+
+    @Override
+    public int deleteAll() {
+       int rowsAffected = 0; 
+        
+        try{  
+            
+        Class.forName(driver);
+             // create a sql date object so we can use it in our INSERT statement
+             try (Connection conn = DriverManager.getConnection(url, user, pw)) {
+                 // create a sql date object so we can use it in our INSERT statement
+                 
+                 // the mysql insert statement
+                 String sqlQuery = "delete from klant";                         
+                 
+                 // create the mysql insert preparedstatement
+                 PreparedStatement preparedStmt = conn.prepareStatement(sqlQuery);
+                                                 
+                 // execute the preparedstatement
+                 rowsAffected = preparedStmt.executeUpdate();                    
+            }
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }    
+        
+      return rowsAffected; 
+    }
+
+    @Override /// nog doen
+    public Klant updateGegevens(Klant klant) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
+    @Override // nog doen: AANPASSEN!
+    public Klant insertKlant(Klant klant) {
+        
+        int klantId = 0;        
+        
+        String voornaam = klant.getVoornaam();
+        String achternaam = klant.getAchternaam();        
+        String tussenvoegsel = klant.getTussenvoegsel();
+        String email = klant.getEmail();
+        
+        // the mysql insert statement
+        String sqlQuery = "insert into Klant (voornaam, achternaam, tussenvoegsel, email)"
+                         + " values (?, ?, ?, ?)";
+    
+        try{ 
+        // create a mysql database connection      
+        Class.forName(driver);
+        // create a sql date object so we can use it in our INSERT statement
+        try (Connection conn = DriverManager.getConnection(url, user, pw);
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt  = conn.prepareStatement(sqlQuery,
+                Statement.RETURN_GENERATED_KEYS) ) {
+                 
+                preparedStmt.setString (1, voornaam);
+                preparedStmt.setString (2, achternaam);
+                preparedStmt.setString (3, tussenvoegsel);
+                preparedStmt.setString (4, email);
+                 // execute the preparedstatement
+                 
+                 int affectedRows = preparedStmt.executeUpdate();
+                 if (affectedRows == 0) {
+                    throw new SQLException("Creating user failed, no rows affected.");
+                 } 
+                 
+                 rs = preparedStmt.getGeneratedKeys();
+                 if (rs.isBeforeFirst()){
+                    if (rs.next()) 
+                        klantId = rs.getInt(1);                         
+                 } 
+                 
+                klantBuilder.klantId(klantId);
+                klantBuilder.voornaam(voornaam);
+                klantBuilder.achternaam(achternaam);
+                klantBuilder.tussenvoegsel(tussenvoegsel);
+                klantBuilder.email(email);
+                 
+                klant = klantBuilder.build();
+         }     
+        }
+        catch (SQLException | ClassNotFoundException ex){
+            System.err.println("Got an exception!");
+            System.err.println(ex.getMessage());
+        }
+    return klant;
+    
+    }
+    
+    @Override // methode wordt niet gebruikt. verwijder uit alle klant klassen en interface
     public boolean deleteByKlantNaam(String achternaam, String tussenvoegsel, String voornaam) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public int deleteAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Klant updateGegevens(Klant klant) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
