8fe838e4c881372d22b9d5f4017a483a04658612
= <anne.hoogerbrugge@gmail.com>
2016-08-25 17:19:19 +0200

    begin crud acties

---
+++ Documents/NetBeansProjects/WebshopJavaApplicatie/src/main/java/DAOs/AdresDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAOs;
+
+import POJO.Adres;
+
+/**
+ *
+ * @author Anne
+ */
+public class AdresDao extends GenericDaoImpl <Adres, Long> {
+    
+    public AdresDao() {
+        super();
+    }
+}
---
+++ Documents/NetBeansProjects/WebshopJavaApplicatie/src/main/java/DAOs/KlantDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAOs;
+
+import POJO.Klant;
+
+/**
+ *
+ * @author Anne
+ */
+public class KlantDao extends GenericDaoImpl <Klant, Long>{
+    
+    public KlantDao() {
+        super();
+    }
+}
